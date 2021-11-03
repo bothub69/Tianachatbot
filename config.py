3d91535437969763b38b28e1d85be1c49ca981bd
bothub69 <88327914+bothub69@users.noreply.github.com>
2021-11-03 09:56:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pyrogram import Client
 
 API_ID = int(os.environ.get("API_ID", "7211896"))
 API_HASH = os.environ.get("API_HASH", "d22a4d25860c6673209ea07dc194857a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", 2064609555:AAGeElYciRg-nIKYEsH-53zZkklxB3oyt54)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "2064609555:AAGeElYciRg-nIKYEsH-53zZkklxB3oyt54")
 ARQ_API_KEY = "PCGCCA-FUAEEL-DZPGGH-ZESZXF-ARQ" 
 LANGUAGE = "en"
 ARQ_API_BASE_URL = "https://thearq.tech"
